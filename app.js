68ed0538ec3fdc5b96541aa33d64593a0d9ad4da
HectorERS <72960108+HectorERS@users.noreply.github.com>
2021-09-21 22:23:20 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -15,9 +15,10 @@ redisClient.on('connect', function(){
 });
 // Verificar el cache, redis
 //redisClient.set("key1", "soy la key1");
-redisClient.get("key1", function(err, value) {
-  console.log(value);
-});
+//redisClient.get("key1", function(err, value) {
+// console.log(value);
+//});
+
 
 
 
@@ -29,15 +30,29 @@ redisClient.get("key1", function(err, value) {
 // Assign route
 app.use('/', (req, res, next) => {
   const filters = req.query;
-  const filteredUsers = data.filter(user => {
-    let isValid = true;
-    for (key in filters){
-      console.log(key, user[key], filters[key]);
-      isValid = isValid && user[key] == filters[key];
-    }
-    return isValid;
+  const {id} = req.query;
+  console.log(id);
+  redisClient.exists(id, function(err, reply){
+    if (reply === 1){
+      redisClient.get(id, function(err, value) {
+      //console.log(value);
+      console.log("fui al cache");
+      res.send(value);
+      });
+    } else {
+      const filteredUsers = data.filter(user => {
+        let isValid = true;
+        for (key in filters){
+          console.log(key, user[key], filters[key]);
+          isValid = isValid && user[key] == filters[key];
+        };
+        return isValid;
+      });
+      console.log(filters);
+      console.log("fui al inventario");
+      res.send(filteredUsers);
+    };
   });
-  res.send(filteredUsers);
 });
 //antes de buscar directamente en el archivo data, debemos conectar y preguntar en el cache redis
 
